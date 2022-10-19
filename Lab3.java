16567a4a5a3703c3aa7609455267078b431db937
electricchef <electriccheff@gmail.com>
2022-10-19 14:56:15 -0500

    Lab3.java which reads from data file for questionOne

--- Lab3.java
+++ Lab3.java
@@ -1,7 +1,41 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Lab3 {
 
     public static void main(String[] args) {
 
     }//end main
+
+
+    public static int questionOne() {
+
+        try {
+
+            FileReader reader = new FileReader("data/lab3Data.txt");
+            Scanner fileParser = new Scanner(reader);
+            while (fileParser.hasNextLine()) {
+
+                String line = fileParser.nextLine();
+                Scanner lineParser = new Scanner(line);
+                lineParser.useDelimiter("|");
+                String provider = lineParser.next();
+                String customer = lineParser.next();
+
+
+            }//end while
+
+
+        }
+        catch (FileNotFoundException fnf) {
+
+            System.out.println(fnf.getMessage());
+
+        }
+
+        return 1;
+
+    }//end questionOne
 
 }//end class
